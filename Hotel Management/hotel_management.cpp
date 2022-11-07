32bf1f49ffe12293996140b3e5f2aca1415a5674
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-11-07 15:25:32 +0530

    adding food items price

--- Hotel Management/hotel_management.cpp
+++ Hotel Management/hotel_management.cpp
@@ -6,7 +6,7 @@ public:
     int quantity;
     int choice;
     // availabilty of items
-    float Qrooms = 10, Qpasta = 0, Qburger = 0;
+    float Qrooms = 10, Qpasta = 0, Qburger = 10;
     float Srooms = 0, Spasta = 0, Sburger = 0;
 
     float Trooms = 0, Tpasta = 0, Tburger = 0;
@@ -14,7 +14,7 @@ public:
 public:
     int availability()
     {
-        std::cout << "\nEnter the availabe rooms in hotel.";
+        std::cout << "\nEnter the available rooms in hotel.";
         std::cin >> Qrooms;
         std::cout << "\nEnter food Details.";
         std::cout << "\nEnter amount of burger available.";
@@ -44,36 +44,60 @@ public:
     void foodServices()
     {
         std::cout << "Below is list of available food items.";
-        std::cout << "\n1. Burger.";
-        std::cout << "\n2. Pasta.";
-        std::cout << "\n3. Sandwich.";
-        std::cout << "\n4. Frenchfries.";
-        std::cout << "\n5. Noodles .";
-        std::cout << "\nEnter your choice";
+        std::cout << "\n1. Burger       -Rs 50.";
+        std::cout << "\n2. Pasta        -Rs 60.";
+        std::cout << "\n3. Sandwich     -Rs 40. ";
+        std::cout << "\n4. Frenchfries  -Rs 30.";
+        std::cout << "\n5. Noodles      -Rs 60.";
+        std::cout << "\nEnter your choice : ";
         std::cin >> choice;
         switch (choice)
         {
         case 1:
+            std::cout << "\nEnter number of Burger you want to order : ";
+            std::cin >> quantity;
+            if (Qburger >= quantity)
+            {
+                std::cout << quantity << " burgers are available, collect your order from counter.";
+                Tburger = quantity * 50;
+                std::cout << "\nTotal cost for " << quantity << " burger is : " << Tburger;
+            }
+            else
+            {
+                std::cout << "\nOnly" << Qburger - quantity << "are available";
+            }
         }
     }
 };
 int main()
 {
-    int choice, quantity;
+    int a, q;
 
     std::cout << "\n1. Customer Services";
     std::cout << "\n2. Admin Login.";
     std::cout << "\n3. Exit";
     HotelManagement h;
     Customer c;
-    std::cout << "\nEnter your choice";
-    std::cin >> choice;
+    std::cout << "\nEnter your choice : ";
+    std::cin >> a;
 
-    switch (choice)
+    switch (a)
     {
     case 1:
-
-        c.rooms();
+        std::cout << "\nSelect services. ";
+        std::cout << "\n1. Room Booking. ";
+        std::cout << "\n2. Food ordering. ";
+        std::cout << "\nEnter your choice : ";
+        std::cin >> q;
+        switch (q)
+        {
+        case 1:
+            c.rooms();
+            break;
+        case 2:
+            c.foodServices();
+            break;
+        }
         break;
     case 2:
         h.availability();
