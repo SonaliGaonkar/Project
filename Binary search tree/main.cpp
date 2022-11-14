ddce6537ef9aa45044e4e8a17a2fe797baca8138
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-11-14 10:29:52 +0530

    created two new files tree.h and tree.cpp

--- Binary search tree/main.cpp
+++ Binary search tree/main.cpp
@@ -1,15 +1,17 @@
+#include "tree.h"
+
 int main()
 {
     int choice = 0, d;
     std::cout << "\n\n1. Add a node";
     std::cout << "\n\n2. Delete a node";
 
-    std::cout << "\n\nEnter your choice";
+    std::cout << "\n\nEnter your choice : ";
     std::cin >> choice;
     switch (choice)
     {
     case 1:
-        std::cout << "Please insert node data";
+        std::cout << "Please insert node data : ";
         std::cin >> d;
     }
 }
---
+++ Binary search tree/tree.h
@@ -0,0 +1,10 @@
+#include "node.h"
+
+class Tree
+{
+public:
+    std::vector <int> v;
+    Node *root;
+    Tree();
+    void insertNode(int);
+};
