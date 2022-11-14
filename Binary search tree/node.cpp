9a21838f1dc8b142fe068166dcfc3d9d96e671d8
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-11-14 10:20:05 +0530

    created two files node.cpp and node.h

---
+++ Binary search tree/node.cpp
@@ -0,0 +1,16 @@
+#include "node.h"
+
+Node::Node()
+{
+    left = nullptr;
+    right = nullptr;
+    data = 0;
+}
+
+Node::Node(int d)
+{
+    left = nullptr;
+    right = nullptr;
+    data = d;
+}
+
---
+++ Binary search tree/node.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node();
+    Node(int);
+};
