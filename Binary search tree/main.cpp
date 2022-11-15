904a68659434506e9e81bd971a0c7f816d4b0a85
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-11-15 10:25:49 +0530

    added code to insert more than one node

--- Binary search tree/main.cpp
+++ Binary search tree/main.cpp
@@ -1,9 +1,8 @@
 #include "tree.cpp"
-
-int main()
+Tree t;
+int choice = 0, d, n;
+void myData()
 {
-    Tree t;
-    int choice = 0, d;
     std::cout << "\n\n1. Add a node";
     std::cout << "\n\n2. Delete a node";
 
@@ -12,9 +11,16 @@ int main()
     switch (choice)
     {
     case 1:
-        std::cout << "Please insert node data : ";
-        std::cin >> d;
-        t.insertNode(d);
+        std::cout << "How many values do you want enter : ";
+        std::cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            std::cout << "Please enter node data :";
+            std::cin >> d;
+            t.insertNode(d);
+        }
+        std::cout << "Would you like to perform any other task ";
+        myData();
         break;
 
     case 2:
@@ -22,4 +28,8 @@ int main()
         std::cin >> d;
         t.root = t.deleteNode(t.root, d);
     }
+}
+int main()
+{
+    myData();
 }
